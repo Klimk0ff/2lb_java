7c6594db7d012590124c414b897636e13d3d353f
Klimk0ff <klimkoff@i.ua>
2022-11-15 16:50:08 +0200

    .

---
+++ examples/ex2_1.java
@@ -0,0 +1,14 @@
+﻿package examples;
+
+public class ex2_1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println( " 0 ");
+            throw new RuntimeException( " Неперевірена помилка");
+
+        } catch (RuntimeException e) { // виняток перехоплено
+            System.out.println( " 1 " +e); // виняток опрацьовано
+        }
+        System.out.println(" 2 ");
+    }
+}
---
+++ examples/ex2_8.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class ex2_8 { public static int m () {
+    try{
+        System.out.println("0");
+        throw new RuntimeException();
+    } finally {
+        System.out.println("1");
+    } }
+    public static void main(String[] args){ System.out.println(m());
+    }}
